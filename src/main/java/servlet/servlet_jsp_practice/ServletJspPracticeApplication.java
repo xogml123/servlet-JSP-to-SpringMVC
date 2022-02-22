adfd4b10071099aa61f41e1f241d01fcabc149b3
xogml123 <xogml951@gmail.com>
2022-02-22 16:50:49 +0900

    project Creation

---
+++ src/main/java/servlet/servlet_jsp_practice/ServletJspPracticeApplication.java
@@ -0,0 +1,13 @@
+package servlet.servlet_jsp_practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServletJspPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServletJspPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/servlet/servlet_jsp_practice/ServletJspPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package servlet.servlet_jsp_practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServletJspPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
